1e7a9d6038d494ea48aeded1325289b68cbf2bc3
aybuge <aybugealtay@gmail.com>
2017-02-22 10:55:15 +0100

    Closes #4

---
+++ src/expert_meme/__init__.py
@@ -0,0 +1,11 @@
+__version__='0.1.0.dev'
+
+__title__ = 'expert-meme'
+__description__ = 'tutorial'
+__url__ = 'https://github.com/aybugealtay/expert-meme'
+
+__author__ = 'Aybuge Altay '
+__email__ = 'aybugealtay@gmail.com'
+
+__license__ = 'Apache 2.0 License'
+__copyright__ = 'Copyright (c) 2017 Aybuge Altay'
